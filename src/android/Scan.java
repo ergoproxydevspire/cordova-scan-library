9937afa7a7f86759e3a98d29bdcad24a6a84247c
ergoproxy623 <ergoproxy623@gmail.com>
2023-09-16 19:20:35 +0300

    registr scanconstans class

--- src/android/Scan.java
+++ src/android/Scan.java
@@ -271,34 +271,6 @@ public class Scan extends CordovaPlugin {
 /**
  * Created by jhansi on 15/03/15.
  */
-public class ScanConstants {
-
-    public final static int PICKFILE_REQUEST_CODE = 1;
-    public final static int START_CAMERA_REQUEST_CODE = 2;
-    public final static String OPEN_INTENT_PREFERENCE = "selectContent";
-    public final static String IMAGE_BASE_PATH_EXTRA = "ImageBasePath";
-    public final static int OPEN_CAMERA = 4;
-    public final static int OPEN_MEDIA = 5;
-    public final static String SCANNED_RESULT = "scannedResult";
-    public final static String IMAGE_PATH = Environment
-            .getExternalStorageDirectory().getPath() + "/scanSample";
-
-    public final static String SELECTED_BITMAP = "selectedBitmap";
-}
-
-
-import android.app.Activity;
-        import android.app.AlertDialog;
-        import android.app.FragmentTransaction;
-        import android.content.ComponentCallbacks2;
-        import android.content.pm.PackageManager;
-        import android.graphics.Bitmap;
-        import android.net.Uri;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.widget.Toast;
-
-        import androidx.annotation.NonNull;
 
 /**
  * Created by jhansi on 28/03/15.
